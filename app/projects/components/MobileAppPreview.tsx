98d36d0b24cf00808cbaa6aefc93a69c757f15d2
PiotrPabich <87279370+PiotrPabichCode@users.noreply.github.com>
2024-01-31 21:26:02 +0100

    Delete zoom from mobile view

--- app/projects/components/MobileAppPreview.tsx
+++ app/projects/components/MobileAppPreview.tsx
@@ -1,39 +1,24 @@
 'use client';
 import Image from 'next/image';
-import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import Zoom from 'react-medium-image-zoom';
-import { Controlled as ControlledZoom } from 'react-medium-image-zoom';
 import 'react-medium-image-zoom/dist/styles.css';
 import { type MobileAppPreviewItem } from '../models/MobileAppPreviewItem';
-import { useCallback, useRef, useState } from 'react';
 
 type Props = {
   mobileAppPreviewItems?: MobileAppPreviewItem[];
 };
 
 export default function MobileAppPreview({ mobileAppPreviewItems }: Props) {
-  const [isZoomed, setIsZoomed] = useState(false);
-  const swiperRef = useRef<SwiperRef>(null);
-
-  const handleZoomChange = useCallback((shouldZoom: boolean) => {
-    setIsZoomed(shouldZoom);
-    if (shouldZoom) {
-      swiperRef.current?.swiper.autoplay.pause();
-    } else {
-      swiperRef.current?.swiper.autoplay.resume();
-    }
-  }, []);
-
   return (
     mobileAppPreviewItems && (
       <>
         <div className='flex sm:hidden items-center justify-center'>
           <Swiper
-            ref={swiperRef}
             grabCursor
             autoplay
             loop
@@ -47,12 +32,11 @@ export default function MobileAppPreview({ mobileAppPreviewItems }: Props) {
                 <SwiperSlide
                   key={`AppPreviewSlide${id}`}
                   className='w-full items-center justify-center self-center'>
-                  <ControlledZoom
-                    isZoomed={isZoomed}
-                    onZoomChange={handleZoomChange}
-                    key={`AppPreviewImage${id}`}>
-                    <Image src={item.image} alt={item.alt} />
-                  </ControlledZoom>
+                  <Image
+                    key={`AppPreviewImage${id}`}
+                    src={item.image}
+                    alt={item.alt}
+                  />
                 </SwiperSlide>
               );
             })}
