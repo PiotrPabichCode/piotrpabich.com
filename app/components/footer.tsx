39bae62da150f19d6d33185fc1b90fcef6d58535
Piotr Pabich <87279370+PiotrPabichCode@users.noreply.github.com>
2025-07-26 18:01:17 +0200

    Update footer to 2025

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,11 @@
-import Divider from './divider';
+import Divider from "./divider";
 
 export default function Footer() {
   return (
     <>
       <Divider />
-      <footer className='py-6'>
-        <p className='text-white text-center'>© 2024 Piotr Pabich</p>
+      <footer className="py-6">
+        <p className="text-white text-center">© 2025 Piotr Pabich</p>
       </footer>
     </>
   );
